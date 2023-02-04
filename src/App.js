732e432119c6390a81b099995a79cc778e1f80f0
Victor López <vmlopez@slashweb.com.mx>
2023-02-04 17:58:42 -0600

    Adding first steps, connection with metamask and initial data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import Home from './views/home'
 import {Route, Routes} from 'react-router-dom';
-import {useEffect} from "react";
-import Web3 from "web3";
+import MainLayout from "./layouts/main";
 
 function App() {
-  useEffect(() => {
-    if (window.ethereum) {
-      // window.ethereum.request({
-      //   method: 'eth_requestAccounts'
-      // }).then(accounts => console.log(accounts))
-      const web3 = new Web3(window.ethereum)
-      web3.eth.requestAccounts().then(acc => console.log(acc))
-    }
-  })
 
   return (
-
-    <Routes>
-      <Route path="/" exact element={<Home/>}/>
-    </Routes>
-
+    <MainLayout>
+      <Routes>
+        <Route path="/" exact element={<Home/>}/>
+      </Routes>
+    </MainLayout>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { HashRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import {Web3ReactProvider} from "@web3-react/core";
+import { getLibrary } from "./config/web3";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
     <HashRouter>
       <ChakraProvider>
-        <App />
+        <Web3ReactProvider getLibrary={getLibrary} >
+          <App />
+        </Web3ReactProvider>
       </ChakraProvider>
     </HashRouter>
   </React.StrictMode>
